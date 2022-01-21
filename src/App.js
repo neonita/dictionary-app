d9c909449b5c3fc296dc384b8fdbe457733f7f57
neonita <neonita888@gmail.com>
2022-01-21 12:50:09 -0500

    removed unecessary lines and files of the default React app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
+      <header className="App-header">
+        <h1>epeolatry</h1>
+        <h3>(ˌɛpɪˈɒlətrɪ)</h3>
+        <h5>the worship of words</h5>
+      </header>
     </div>
   );
 }
