7a6302b3eeeed59ad95fafbc8fc1c480c32ebf67
neonita <neonita888@gmail.com>
2022-01-21 20:14:53 -0500

    added a search engine which retains the input value and updates each time a new value is computed

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import "./SearchEngine.css";
 
 export default function SearchEngine() {
-  return <>search engine</>;
+  const [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`${keyword}`);
+  }
+
+  function searchKeyword(event) {
+    let updateValue = event.target.value;
+    console.log(updateValue);
+    setKeyword(updateValue);
+  }
+
+  return (
+    <div className="SearchEngine">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Search for a definition..."
+          onChange={searchKeyword}
+        />
+      </form>
+    </div>
+  );
 }
