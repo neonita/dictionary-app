1e66dfdff1ea54c71ebf773cdbc3beddd381c713
neonita <neonita888@gmail.com>
2022-01-21 21:26:00 -0500

    added dictionary api integration

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
   const [keyword, setKeyword] = useState("");
 
+  function displayWord(response) {
+    let definition = response.data[0];
+    console.log(definition);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`${keyword}`);
+
+    const API_URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(API_URL).then(displayWord);
   }
 
   function searchKeyword(event) {
