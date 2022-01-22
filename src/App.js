a5d9c425b44b79e5ae0de692dbbbd40c6d701970
neonita <neonita888@gmail.com>
2022-01-21 19:16:47 -0500

    added url to anchor links in the footer tag and created a search engine component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import "./App.css";
+import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>epeolatry</h1>
-        <h3>(ˌɛpɪˈɒlətrɪ)</h3>
-        <h5>the worship of words</h5>
+        <h5>(ˌɛpɪˈɒlətrɪ)</h5>
+        <h6>the worship of words</h6>
       </header>
+      <SearchEngine />
+      <footer className="App-footer">
+        <small>
+          © 2022 Melissa Banoen-Garde, open-sourced on{" "}
+          <a
+            href="https://github.com/neonita/dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://epeolatry.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
